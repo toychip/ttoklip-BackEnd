696201989a21a38e95ec379bea596e0eeb0e3cdb
SaeJoon <psae0714@naver.com>
2024-01-30 22:10:10 +0900

    feat:#2 MemberRepository 변경 email->userEmail, Email 메소드들 UserEmail로 변경

--- src/main/java/com/api/ttoklip/domain/Login/main/oauth/service/CustomOauth2Service.java
+++ src/main/java/com/api/ttoklip/domain/Login/main/oauth/service/CustomOauth2Service.java
@@ -1,16 +1,22 @@
 package com.api.ttoklip.domain.login.main.oauth.service;
 
+import com.api.ttoklip.domain.login.main.oauth.principal.CustomOAuth2User;
 import com.api.ttoklip.domain.login.main.oauth.userinfo.OAuth2UserInfo;
 import com.api.ttoklip.domain.login.main.oauth.userinfo.OAuthUserInfoFactory;
+import com.api.ttoklip.domain.member.domain.Member;
+import com.api.ttoklip.domain.member.domain.Role;
 import com.api.ttoklip.domain.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
@@ -20,6 +26,9 @@ public class CustomOauth2Service extends DefaultOAuth2UserService {
 
     private final MemberRepository memberRepository;
 
+    private @Lazy PasswordEncoder passwordEncoder;
+
+    @Override
     public OAuth2User loadUser(OAuth2UserRequest request)throws OAuth2AuthenticationException{
 
         OAuth2User oAuth2User = super.loadUser(request);
@@ -27,12 +36,24 @@ public class CustomOauth2Service extends DefaultOAuth2UserService {
 
         String provider = request.getClientRegistration().getRegistrationId();
 
+        //어떤 소셜로그인인지 구분
         OAuth2UserInfo oAuth2UserInfo = OAuthUserInfoFactory.getOAuthUserInfo(provider,oAuth2User.getAttributes());
         //회원가입 유무 확인
-        //Optional<Member> member=memberRepository.find
-        /*if(member.isEmpty()){
+        Optional<Member> member=memberRepository.findMemberByUserEmailAndProvider(oAuth2UserInfo.getEmail(),provider);
+
+        if(member.isEmpty()){
             Member newMember=Member.builder()
-        }*/
-        return null;
+                    .userNickname(oAuth2UserInfo.getName())
+                    .userEmail(oAuth2UserInfo.getEmail())
+                    .provider(provider)
+                    .password(passwordEncoder.encode("oauth"))
+                    .role(Role.GENERAL)
+                    .build();
+
+            memberRepository.save(newMember);//변경된 일반 유저 저장
+
+            return new CustomOAuth2User(newMember, oAuth2User.getAttributes());
+        }
+        return new CustomOAuth2User(member.get(),oAuth2User.getAttributes());
     }
 }
--- src/main/java/com/api/ttoklip/domain/member/repository/MemberRepository.java
+++ src/main/java/com/api/ttoklip/domain/member/repository/MemberRepository.java
@@ -1,4 +1,15 @@
 package com.api.ttoklip.domain.member.repository;
 
-public class MemberRepository {
+import com.api.ttoklip.domain.member.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByUserEmail(String userEmail);
+    List<Member> findMemberByUserEmail(String userEmail);
+    Optional<Member> findMemberByUserEmailAndProvider(String userEmail,String Provider);
+    Optional<Member> findById(Long userId);
+    Optional<Member> findByUserEmailAndUserNickname(String userEmail, String provider);
 }

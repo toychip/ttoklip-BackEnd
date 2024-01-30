535a2df460d5e5b8ad2df0530db7c74cf25f1dde
SaeJoon <psae0714@naver.com>
2024-01-30 15:36:22 +0900

    feat:#2 Oauth록그인 handler, principle 추가 principle작성

---
+++ src/main/java/com/api/ttoklip/domain/Login/main/oauth/handler/OAuth2AuthenticationSuccessHandler.java
@@ -0,0 +1,13 @@
+package com.api.ttoklip.domain.login.main.oauth.handler;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class OAuth2AuthenticationSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
+
+}
---
+++ src/main/java/com/api/ttoklip/domain/Login/main/oauth/principal/OAuthUser.java
@@ -0,0 +1,47 @@
+package com.api.ttoklip.domain.login.main.oauth.principal;
+
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+
+import java.lang.reflect.Member;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Map;
+@Data
+public class OAuthUser implements OAuth2User {
+    private Member user;
+    private Map<String, Object> attributes;
+
+    public OAuthUser(Member user, Map<String, Object> attributes){
+        this.user=user;
+        this.attributes=attributes;
+    }
+
+    @Override
+    public <A> A getAttribute(String name){
+        return OAuth2User.super.getAttribute(name);
+    }
+
+    @Override
+    public Map<String, Object> getAttributes() {
+        return attributes;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singleton(new SimpleGrantedAuthority("ROLE_MEMBER"));
+    }
+
+    @Override
+    public String getName() {
+        if(user.getProvider().equals("kakao")){
+            return ((Map<?,?>) attributes.get("properties")).get("nickName").toString();
+        }else if (user.getProvider().equals("naver")){
+            return ((Map<?,?>) attributes.get("response")).get("nickName").toString();
+        }
+
+        return null;
+    }
+}

fb779e2399561bd02ada012c0a5fa678a668b0c7
SaeJoon <psae0714@naver.com>
2024-02-01 16:22:02 +0900

    feat:#20 에러타입 추가 usernotfound oauth handller제외하고 구현

--- src/main/java/com/api/ttoklip/domain/member/repository/MemberRepository.java
+++ src/main/java/com/api/ttoklip/domain/member/repository/MemberRepository.java
@@ -2,14 +2,15 @@ package com.api.ttoklip.domain.member.repository;
 
 import com.api.ttoklip.domain.member.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findByUserEmail(String userEmail);
     List<Member> findMemberByUserEmail(String userEmail);
     Optional<Member> findMemberByUserEmailAndProvider(String userEmail,String Provider);
-    Optional<Member> findById(Long userId);
     Optional<Member> findByUserEmailAndUserNickname(String userEmail, String provider);
 }
